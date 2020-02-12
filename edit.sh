3d9061ebc66867679a0605af391222ea7b2a6bc0
David <12832280+David-Else@users.noreply.github.com>
2020-02-12 11:24:40 +0000

    Update to use pnpm interactive update and refactor

--- edit.sh
+++ edit.sh
@@ -3,26 +3,29 @@
 #=======================================================================================
 #         FILE: edit.sh
 #        USAGE: edit.sh
-#  DESCRIPTION: Utility to allow selection of a project directory with the option to
-#               check/update the dependencies, run Rollup watch, and edit in VS Code
+#  DESCRIPTION: Utility to allow selection of a project to edit with the option to
+#               check/update the dependencies, run Rollup watch, and edit in VS Code.
+#
+#               Directory structure in PROJECT_FOLDER should have two levels:
+#
+#               ├── language-name
+#               │   ├── project-name
+#
 #      WEBSITE: https://www.elsewebdevelopment.com/
-# REQUIREMENTS: npm-check   https://www.npmjs.com/package/npm-check
+# REQUIREMENTS: pnpm        https://github.com/pnpm/pnpm
 #               rollup      https://www.npmjs.com/package/rollup
 #               VS Code     https://code.visualstudio.com/
 #       AUTHOR: David Else
 #      COMPANY: Else Web Development
-#      VERSION: 1.0
-#     REVISION: 03-01-18
+#      VERSION: 1.1
+#     REVISION: 12-02-20
 #=======================================================================================
 
 PROJECT_FOLDER="$HOME/sites" # site that contains users projects to edit
 BOLD=$(tput bold)            # bold text
 NORMAL=$(tput sgr0)          # normal text
 
-#===  FUNCTION  ========================================================================
-#         NAME:  create_menu_from_directory
-#  DESCRIPTION:  convert current directory names into a menu for selection
-#=======================================================================================
+# Convert current directory names into a menu for selection
 function create_menu_from_directory() {
     IFS=$'\n' read -d '' -ra files < <(ls -td -- */) # sorted names of dirs in ${files[@]}
 
@@ -32,9 +35,6 @@ function create_menu_from_directory() {
     done
 }
 
-#---------------------------------------------------------------------------------------
-# ask the user which project they would like to edit
-#---------------------------------------------------------------------------------------
 cd "$PROJECT_FOLDER" || exit
 
 clear
@@ -45,29 +45,27 @@ echo
 
 create_menu_from_directory
 cd "$f" || exit
+create_menu_from_directory
+cd "$f" || exit
+echo "${BOLD}"
+pwd
+echo "${NORMAL}"
 
-#---------------------------------------------------------------------------------------
-# check NPM packages are up-to-date and allow upgrading using npm-check
-#---------------------------------------------------------------------------------------
 if [[ -f package.json ]]; then
-    echo
     echo "${BOLD}'package.json' detected${NORMAL}"
     echo
-    read -p "Check NPM packages are up-to-date? (y/n) " -n 1 -r
+    read -p "Check pnpm packages are up-to-date? (y/N) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        npm-check -u
+        pnpm update --interactive
     fi
 fi
 
-#---------------------------------------------------------------------------------------
-# ask user if they would like to run Rollup if it is detected
-#---------------------------------------------------------------------------------------
-if [[ -f rollup.config.js ]]; then
+if [[ -f ./node_modules/.bin/rollup ]]; then
     echo
-    echo "${BOLD}'rollup.config.js' detected${NORMAL}"
+    echo "${BOLD}Rollup detected locally${NORMAL}"
     echo
-    read -p "Run Rollup in watch mode? (y/n) " -n 1 -r
+    read -p "Run Rollup in watch mode? (y/N) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         code .
@@ -76,4 +74,5 @@ if [[ -f rollup.config.js ]]; then
     fi
 fi
 
-code .
+# GTK_IM_MODULE=ibus is workaround for Centos bug that stops some VS Code shortcuts working
+GTK_IM_MODULE=ibus code .
